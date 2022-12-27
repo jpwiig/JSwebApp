2e28b7d2adf4c2e1dffcd883cf2793e0be31374e
jpwiig <jpwiig@gmail.com>
2022-12-27 14:51:50 +0100

    test init

--- jswebapp/src/App.js
+++ jswebapp/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import first from './index/first';
 
 function App() {
   return (
@@ -7,6 +8,14 @@ function App() {
       <header className="App-header">
       <h1>Welcome to a fully build javascript web application</h1>
       <p>here we use React to build a fancy app</p>
+      <ol>
+    <ul>example 1</ul>
+        <ul>example 2</ul>
+        <ul>example 3</ul>
+        <ul>example 4</ul>
+      </ol>
+
+      <h2 id="techtxt"> tech stack used</h2>
       <img src= {logo} id = "ReactLogo" alt = ""></img>
         <a
           className="App-link"
